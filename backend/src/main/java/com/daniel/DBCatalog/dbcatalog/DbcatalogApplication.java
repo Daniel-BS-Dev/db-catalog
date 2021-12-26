bc6494b427aedb472512ef787512c11a7412141a
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-12-26 17:32:29 -0300

    Backend Created

---
+++ backend/src/main/java/com/daniel/DBCatalog/dbcatalog/DbcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.DBCatalog.dbcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbcatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/daniel/DBCatalog/dbcatalog/DbcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.DBCatalog.dbcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
