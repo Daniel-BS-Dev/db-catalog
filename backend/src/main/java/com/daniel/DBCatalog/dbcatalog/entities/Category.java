ac0cd15e60c50217fe21840c53243dde76ca2ada
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-12-26 19:17:01 -0300

    Relationship jpa

--- backend/src/main/java/com/daniel/DBCatalog/dbcatalog/entities/Category.java
+++ backend/src/main/java/com/daniel/DBCatalog/dbcatalog/entities/Category.java
@@ -4,12 +4,24 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
+	@ManyToMany(mappedBy = "categories")
 	private List<Product> products = new ArrayList<>();
 	
 	public Category() {
--- backend/src/main/java/com/daniel/DBCatalog/dbcatalog/entities/Product.java
+++ backend/src/main/java/com/daniel/DBCatalog/dbcatalog/entities/Product.java
@@ -4,8 +4,21 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_product")
 public class Product {
 	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String description;
@@ -13,6 +26,10 @@ public class Product {
 	private String imgUrl;
 	private Instant date;
 	
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", 
+	joinColumns = @JoinColumn(name = "product_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	Set<Category> categories = new HashSet<>();
 
 	public Product() {
