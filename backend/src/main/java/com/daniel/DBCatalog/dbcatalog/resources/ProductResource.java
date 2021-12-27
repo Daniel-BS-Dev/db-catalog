444446444f1f0ffbaaed62d16789f5719f5ae89f
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-12-27 10:18:19 -0300

    Request Product Get By Id

--- backend/src/main/java/com/daniel/DBCatalog/dbcatalog/resources/ProductResource.java
+++ backend/src/main/java/com/daniel/DBCatalog/dbcatalog/resources/ProductResource.java
@@ -6,10 +6,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.daniel.DBCatalog.dbcatalog.dto.CategoryDTO;
 import com.daniel.DBCatalog.dbcatalog.dto.ProductDTO;
 import com.daniel.DBCatalog.dbcatalog.services.ProductService;
 
@@ -33,5 +35,11 @@ public class ProductResource {
 		Page<ProductDTO> list = service.findAllPage(pageRequest);
 	
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> finById(@PathVariable Long id) {
+		ProductDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
 	}
 }
--- backend/src/main/java/com/daniel/DBCatalog/dbcatalog/services/ProductService.java
+++ backend/src/main/java/com/daniel/DBCatalog/dbcatalog/services/ProductService.java
@@ -1,5 +1,7 @@
 package com.daniel.DBCatalog.dbcatalog.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,18 +11,24 @@ import org.springframework.transaction.annotation.Transactional;
 import com.daniel.DBCatalog.dbcatalog.dto.ProductDTO;
 import com.daniel.DBCatalog.dbcatalog.entities.Product;
 import com.daniel.DBCatalog.dbcatalog.repositories.ProductRepository;
+import com.daniel.DBCatalog.dbcatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ProductService {
-	
+
 	@Autowired
 	private ProductRepository repository;
-	
-    @Transactional(readOnly= true)
+
+	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAllPage(PageRequest pageRequest) {
-        Page<Product> list = repository.findAll(pageRequest);
+		Page<Product> list = repository.findAll(pageRequest);
 		return list.map(x -> new ProductDTO(x));
 	}
 
-
+	@Transactional(readOnly = true)
+	public ProductDTO findById(Long id) {
+		Optional<Product> obj = repository.findById(id);// essa exceção esta sendo personalizada na class StandardError
+		Product entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ProductDTO(entity, entity.getCategories());
+	}
 }
