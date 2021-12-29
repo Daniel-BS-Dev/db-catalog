9b83d5afcbe1c4e10e7d1ed85504527a773422bc
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-12-29 20:53:44 -0300

    navBar part 2 button navbar

--- front-web/src/components/NavBar/index.tsx
+++ front-web/src/components/NavBar/index.tsx
@@ -1,17 +1,31 @@
 import './styles.scss';
-
+import 'bootstrap/js/src/collapse.js';
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-md bg-primary nav-container">
+    <nav className="navbar navbar-expand-md navbar-dark bg-primary nav-container">
       <div className="container-fluid">
         <a href="home" className="nav-logo-text">
           <h4>DB CATALOG</h4>
         </a>
-        <div className="nav-container-link collapse navbar-collapse">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#dscatalog-navbar"
+          aria-controls="dscatalog-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="nav-container-link collapse navbar-collapse" id="dscatalog-navbar">
           <ul className="navbar-nav offset-md-2 main-menu">
             <li>
-              <a href="home" className="active">HOME</a>
+              <a href="home" className="active">
+                HOME
+              </a>
             </li>
             <li>
               <a href="Catalog">Catálogo</a>
