d4076c5359646ff9d2f85dd11588583c8a7a18a4
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-12-31 09:32:17 -0300

    CardProduct

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -2,16 +2,33 @@ import CardProduct from 'pages/CardProduct';
 import './styles.scss';
 
 const Catalog = () => {
-    return (
-     <div className='catalog-container'>
-       <div className='row catalog-content'>
-          <div className='.col-12 col-sm-4 col-md-3 catalog-product'><CardProduct/></div>
-          <div className='.col-12 col-sm-4 col-md-3 catalog-product'><CardProduct/></div>
-          <div className='.col-12 col-sm-4 col-md-3 catalog-product'><CardProduct/></div>
-          <div className='.col-12 col-sm-4 col-md-3 catalog-product'><CardProduct/></div>
-       </div>
-     </div>
-    );
-}
+  return (
+    <div className="catalog-container">
+      <div className="row catalog-content">
+        <div className="col-12 col-sm-4 col-md-3 col-lg-2 catalog-product">
+          <CardProduct />
+        </div>
+        <div className="col-12 col-sm-4 col-md-3 col-lg-2 catalog-product">
+          <CardProduct />
+        </div>
+        <div className="col-12 col-sm-4 col-md-3 col-lg-2 catalog-product">
+          <CardProduct />
+        </div>
+        <div className="col-12 col-sm-4 col-md-3 col-lg-2 catalog-product">
+          <CardProduct />
+        </div>
+        <div className="col-12 col-sm-4 col-md-3 col-lg-2 catalog-product">
+          <CardProduct />
+        </div>
+        <div className="col-12 col-sm-4 col-md-3 col-lg-2 catalog-product">
+          <CardProduct />
+        </div>
+        <div className="col-12 col-sm-4 col-md-3 col-lg-2 catalog-product">
+          <CardProduct />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Catalog;
