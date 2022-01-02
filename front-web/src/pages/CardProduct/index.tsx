ec2b7ba47e2bf107487924a2a48ec87bef60fb83
Daniel-BS-Dev <danielbenedito355@gmail.com>
2022-01-02 13:55:09 -0300

    Product list

--- front-web/src/pages/CardProduct/index.tsx
+++ front-web/src/pages/CardProduct/index.tsx
@@ -1,45 +1,23 @@
-import { AxiosParams } from 'types/vendor/axios';
-import { SpringPage } from 'types/vendor/spring';
-import { useEffect, useState } from 'react';
-import { BASE_URL } from 'util/request';
 import { Product } from 'types/product';
 import Price from 'components/Price';
-import axios from 'axios';
 import './styles.scss';
 
-const CardProduct = () => {
-  //SpringPage -> foi o tipo que eu criei no meu vendor
-  const [page, setPage] = useState<SpringPage<Product>>();
+type Props ={
+  product: Product;
+}
 
-  useEffect(() => {
-    //AxiosParams -> foi outor tipo que eu criei no vendor
-    const params: AxiosParams = {
-      method: 'GET',
-      url: `${BASE_URL}/products`,
-      params: {
-        page: 0,
-        size: 12,
-      },
-    };
-    //como eu já coloquei o get no AxiosParams eu não preciso colocar aqui
-    axios(params)
-    .then(response => {
-      setPage(response.data);
-      console.log(page);
-    })
-
-  }, []);
+const CardProduct = ({product} : Props) => {
 
   return (
     <div className="cardProduct-container">
       <div className="cardProduct-content">
         <div className="cardproduct-image">
-          <img src={''} alt="imagem product" />
+          <img src={product.imgUrl} alt={product.name} />
         </div>
         <div className="cardProduct-description">
-          <h6>Computador Desktop - Intel Core i7</h6>
+          <h6>{product.name}</h6>
           <div>
-            <Price price={233.9} />
+            <Price price={product.price} />
           </div>
         </div>
       </div>
--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,17 +1,45 @@
+import { AxiosParams } from 'types/vendor/axios';
+import { SpringPage } from 'types/vendor/spring';
 import Pagination from 'components/Pagination';
 import CardProduct from 'pages/CardProduct';
+import { useEffect, useState } from 'react';
+import { Product } from 'types/product';
+import { BASE_URL } from 'util/request';
+import axios from 'axios';
 import './styles.scss';
 
 const Catalog = () => {
+  //SpringPage -> foi o tipo que eu criei no meu vendor
+  const [page, setPage] = useState<SpringPage<Product>>();
+
+  useEffect(() => {
+    //AxiosParams -> foi outor tipo que eu criei no vendor
+    const params: AxiosParams = {
+      method: 'GET',
+      url: `${BASE_URL}/products`,
+      params: {
+        page: 0,
+        size: 12,
+      },
+    };
+    //como eu já coloquei o get no AxiosParams eu não preciso colocar aqui
+    axios(params).then((response) => {
+      setPage(response.data);
+      console.log(page);
+    });
+  }, []);
+
   return (
     <div className="catalog-container">
       <div className="row catalog-content">
-        <div className="col-12 col-sm-4 col-md-3 col-lg-2 catalog-product">
-          <CardProduct />
-        </div>
-        <div className="col-12 col-sm-4 col-md-3 col-lg-2 catalog-product">
-          <CardProduct />
-        </div>
+        {page?.content.map((product) => (
+          <div
+            className="col-12 col-sm-4 col-md-3 col-lg-2 catalog-product"
+            key={product.id}
+          >
+            <CardProduct product={product} />
+          </div>
+        ))}
       </div>
       <div className="catalog-pagination">
         <Pagination />
