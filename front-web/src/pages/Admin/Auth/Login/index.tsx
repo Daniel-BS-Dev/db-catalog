a6e9838c99dddd7ac38485dcef76b7664254c2a7
Daniel-BS-Dev <danielbenedito355@gmail.com>
2022-01-04 10:26:01 -0300

    Catching data username

--- front-web/src/pages/Admin/Auth/Login/index.tsx
+++ front-web/src/pages/Admin/Auth/Login/index.tsx
@@ -1,22 +1,45 @@
 import Button from 'components/Button';
+import { useForm } from 'react-hook-form';
+import { EphemeralKeyInfo } from 'tls';
 import './styles.scss';
 
+type FormData = {
+  email: string;
+  password: string;
+}
+
 const Login = () => {
+  const {register,handleSubmit, formState: { errors } } = useForm<FormData>();
+
+  const onSubmit = (formData: FormData ) => {
+   console.log(formData);
+  }
+
   return (
     <div className="login-container">
       <div className="login-content">
         <h1>Login</h1>
-        <form>
-          <input type="email" placeholder="Email" className="form-control" />
-          <input type="password" placeholder="Senha" className="form-control login-input-password" />
-        </form>
-        <p className="login-help">Esqueci a senha?</p>
-        <div className="login-button">
-          <Button text="LOGAR" />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            type="email"
+            placeholder="Email"
+            className="form-control"
+            {...register('email', { required: true })}
+          />
+          <input
+            type="password"
+            placeholder="Senha"
+            className="form-control login-input-password"
+            {...register('password', { required: true })}
+          />
+          <p className="login-help">Esqueci a senha?</p>
+          <div className="login-button">
+            <Button text="LOGAR" />
           </div>
-        <p className="login-fillOut">
-          Não tem Cadastro? <span>CADASTRAR</span>
-        </p>
+          <p className="login-fillOut">
+            Não tem Cadastro? <span>CADASTRAR</span>
+          </p>
+        </form>
       </div>
     </div>
   );
