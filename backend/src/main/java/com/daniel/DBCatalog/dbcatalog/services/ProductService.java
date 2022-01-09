c9bfd03305aa2e09d4db8dfea4187025efe7af5b
Daniel-BS-Dev <danielbenedito355@gmail.com>
2022-01-09 16:12:01 -0300

    delete product observer patner

--- backend/src/main/java/com/daniel/DBCatalog/dbcatalog/services/ProductService.java
+++ backend/src/main/java/com/daniel/DBCatalog/dbcatalog/services/ProductService.java
@@ -37,7 +37,7 @@ public class ProductService {
 		List<Category> categories = categoryId == 0 ? null : Arrays.asList(categoryRepository.getById(categoryId));  
 		Page<Product> page = repository.find(categories,name, pageable);
 		repository.findProductsWithCategories(page.getContent());
-		return page.map(x -> new ProductDTO(x));
+		return page.map(x -> new ProductDTO(x, x.getCategories()));
 	}
 
 	@Transactional(readOnly = true)
